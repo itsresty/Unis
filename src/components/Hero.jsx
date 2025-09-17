0a518ae73f72c363dc4d760f281f425952de28e0
Resty Montero <restymontero@Restys-MacBook-Pro.local>
2025-09-17 16:18:30 +0200

    Update

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+export const Hero = () => {
+    return <section>
+        <div>
+            <h1 className="text-4xl font-bold text-center">
+                H1ero Section
+            </h1>
+        </div>
+    </section>
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
+import {Hero} from "../components/Hero.jsx";
+
 export const Home = () => {
     return <div>
         {/* Navbar */}
 
 
         {/* Main Content */}
-
-
+        <main>
+            <Hero/>
+        </main>
         {/* Footer */}
 
 
